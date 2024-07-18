c18804ead9a0c34866f3ff027d094412e611f3de
yearin <year.in.rulu@gmail.com>
2024-07-18 11:05:10 +0900

    BOJ_1300

---
+++ Baekjoon/BOJ_1300/Program.cs
@@ -0,0 +1,28 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int N = Convert.ToInt32(Console.ReadLine());
+        int K = Convert.ToInt32(Console.ReadLine());
+        
+        int start = 1;
+        int end = K;
+        int result = -1;
+        
+        while(start <= end)
+        {
+            int cnt =0;
+            int mid = (start+end)/2;
+            for(int i=1; i<=N; i++)
+                cnt = cnt + Math.Min(mid/i, N);
+            if(cnt<K)
+                start = mid+1;
+            else
+            {
+                result = mid;
+                end = mid-1;
+            }
+        }
+        Console.WriteLine(result);
+    }
+}
