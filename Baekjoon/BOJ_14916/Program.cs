1ce0c059448e1a2a96980b2dca61a29657f1ecdc
yearin <year.in.rulu@gmail.com>
2024-07-25 12:01:45 +0900

    BOJ_14916 추가

---
+++ Baekjoon/BOJ_14916/Program.cs
@@ -0,0 +1,25 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int n = Convert.ToInt32(Console.ReadLine());
+        int count = 0;
+        int result = -1;
+        if(n%5 == 0)
+           result = n/5;
+        else
+        {
+            while(n>0)
+            {
+                n=n-2;
+                count++;
+                if(n%5 == 0)
+                {
+                    result = n/5+count;
+                    break;
+                }
+            }
+        }
+        Console.WriteLine(result);
+    }
+}
