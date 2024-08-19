473e3aab9375cc8cc9ed4872f0af7d29ae6446b4
yearin-rulu <year.in.rulu@gmail.com>
2024-08-19 04:35:38 +0000

    BOJ_4386

---
+++ Baekjoon/BOJ_4386/Program.cs
@@ -0,0 +1,13 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        for(int line=0; line<n; line++)
+        {
+            
+
+        }
+    }
+}
